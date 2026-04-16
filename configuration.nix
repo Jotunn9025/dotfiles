7b425cd20a10089fcd9a4f831ecb37303c3be1f8
Jotunn9025 <youhanlalwani@gmail.com>
2026-04-16 16:22:47 +0530

    Gnome DE

--- configuration.nix
+++ configuration.nix
@@ -46,9 +46,9 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
