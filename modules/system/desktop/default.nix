cd11d468164676da33fd15fc44dc3590a3604c49
Jotunn9025 <youhanlalwani@gmail.com>
2026-04-17 18:58:35 +0530

    Gnome Extensions: Tiling, Weather and hide topbar

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -1,7 +1,9 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 with lib;
-let cfg = config.system.desktop;
+let 
+  cfg = config.system.desktop;
+  hm = inputs.home-manager.lib.hm;
 in {
   options.system.desktop = {
     enable = mkEnableOption "GNOME Desktop Environment, X11, and Printing";
@@ -22,10 +24,63 @@ in {
     # Printing
     services.printing.enable = true;
 
+    # 1. Install the Extension Packages
+    environment.systemPackages = with pkgs; [
+      gnome-extension-manager
+      gnomeExtensions.forge
+      gnomeExtensions.hide-top-bar
+      gnomeExtensions.simpleweather
+    ];
+
     home-manager.users.youhan = {
       dconf.settings = {
         "org/gnome/desktop/interface" = {
           color-scheme = "prefer-dark";
+        };
+
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = [
+            "forge@jmmaranan.com"
+            "hidetopbar@mathieu.bidon.ca"
+            "simple-weather@romanlefler.com"
+          ];
+        };
+
+        "org/gnome/desktop/wm/keybindings" = {
+          maximize = [];
+          unmaximize = [];
+        };
+        "org/gnome/mutter/keybindings" = {
+          toggle-tiled-left = [];
+          toggle-tiled-right = [];
+        };
+        
+        "org/gnome/shell/extensions/forge" = {
+          tiling-mode-enabled = true;
+          window-gap-size = 8;
+          window-gap-hidden-on-single = true;
+          
+          focus-border-toggle = true;
+          focus-border-color = hm.gvariant.mkTuple [ true 0.682 0.733 1.0 1.0 ];
+          focus-border-size = 2; 
+        };
+
+        "org/gnome/shell/extensions/forge/keybindings" = {
+          window-focus-left = ["<Super>Left"];
+          window-focus-right = ["<Super>Right"];
+          window-focus-up = ["<Super>Up"];
+          window-focus-down = ["<Super>Down"];
+
+          window-swap-left = ["<Shift><Super>Left"];
+          window-swap-right = ["<Shift><Super>Right"];
+          window-swap-up = ["<Shift><Super>Up"];
+          window-swap-down = ["<Shift><Super>Down"];
+        };
+
+        "org/gnome/shell/extensions/hidetopbar" = {
+          mouse-sensitive = true;
+          enable-active-window-hints = true;
         };
       };
     };
@@ -33,7 +88,5 @@ in {
     environment.sessionVariables = {
       TERMINAL = "kitty";
     };
-
   };
-
 }
